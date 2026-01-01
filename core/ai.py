ab1468a95aacbfbda6e2916b852414a0477e7aed
laithelwan <laithelwan241@gmail.com>
2026-01-01 05:26:35 +0300

    LAST

--- core/ai.py
+++ core/ai.py
@@ -1,20 +1,2 @@
-from transformers import BlipProcessor, BlipForConditionalGeneration
-from PIL import Image
-import torch
-
-# تحميل النموذج مرة واحدة فقط
-processor = BlipProcessor.from_pretrained(
-    "Salesforce/blip-image-captioning-base"
-)
-model = BlipForConditionalGeneration.from_pretrained(
-    "Salesforce/blip-image-captioning-base"
-)
-
 def describe_image(image_path):
-    image = Image.open(image_path).convert("RGB")
-
-    inputs = processor(image, return_tensors="pt")
-    output = model.generate(**inputs)
-
-    caption = processor.decode(output[0], skip_special_tokens=True)
-    return caption
+    return "تم رفع الصورة بنجاح ✅"
